110b97edd0aea770fe4427728df79231e2302ca6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-10-23 17:55:25 +0100

    Switch to a little express app

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,28 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-exports.bigben = functions.https.onRequest((req, res) => {
-  const hours = (new Date().getHours() % 12) + 1  // London is UTC + 1hr; (lol not in the winter though)
-  res.status(200).send(`<!doctype html>
+app.get('/', (req, res) => {
+  const date = new Date();
+  const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+  res.send(`
+    <!doctype html>
     <head>
       <title>Time</title>
     </head>
     <body>
-      ${'BONG '.repeat(hours)}
+      <p>In London, the clock strikes:
+        <span id="bongs">${'BONG '.repeat(hours)}</span>
+      </p>
     </body>
   </html>`);
 });
+
+app.get('/api', (req, res) => {
+    const date = new Date();
+    const hours = (date.getHours() % 12) + 1;  // London is UTC + 1hr;
+    res.json({bongs: 'BONG '.repeat(hours)});
+});
+
+exports.app = functions.https.onRequest(app);
+
